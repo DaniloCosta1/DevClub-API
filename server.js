ecbed31db052389ee0bfb34c310902b44985ff76
Danilo Costa <danilopscosta@gmail.com>
2024-06-15 16:38:08 -0300

    ok

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ DELETE -> DELETAR
     1) TIPO DE ROTA / MÉTODO HTTP
     2) ENDEREÇO
 */
+
+/*
+    CRIAR UMA API DE USUÁRIOS
+    -Criar um usuário
+    -Listar todos os usuários
+    -Editar um usuário
+*/
+
+const users = [];
+
+app.post('/usuarios', (req, res) => {
+    console.log(req);
+    res.send('ok, aqui deu certo');
+});
+
 app.get('/usuarios', (request, response) => {
     response.send('ok, deu bom')
 });
