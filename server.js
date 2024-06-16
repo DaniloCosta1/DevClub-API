622fe216c18629dff474c88f156401396d21de7a
Danilo Costa <danilopscosta@gmail.com>
2024-06-16 11:35:46 -0300

    ok

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express, { request, response } from 'express'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 const app = express();
 app.use(express.json());
 
@@ -22,16 +24,28 @@ DELETE -> DELETAR
     -Editar um usuário
 */
 
-const users = [];
 
-app.post('/users', (req, res) => {
+
+app.post('/users', async(req, res) => {
     
-    users.push(req.body);
+    await prisma.user.create({
+        data:{
+            email:req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    });
     res.status(201).json(req.body);
 });
 
-app.get('/users', (request, response) => {
-    response.status(200).json(users);
+app.get('/users', async (req, res) => {
+    
+    const users = await prisma.user.findMany();
+
+    res.status(200).json(users);
+
 });
+
+
 
 app.listen(3000);
